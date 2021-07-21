6cae08de071aa7fea9d9edb4d7e3dc41b834671d
fanglingxiao <fanglingxiao@guoquan.cn>
2021-07-21 09:28:17 +0800

    ##classLoader.loadClass

--- src/main/java/com/fang/jvm/test/Test02_ClassLoaderScope.java
+++ src/main/java/com/fang/jvm/test/Test02_ClassLoaderScope.java
@@ -3,13 +3,21 @@ package com.fang.jvm.test;
 /**
  * @author fanglingxiao
  * @version 1.0
- * @description TODO
+ * @description classLoader范围
  * @date 2021/7/13 12:40 上午
  **/
-public class Test02_ClassLoaderLevel {
+public class Test02_ClassLoaderScope {
+    /**
+     * Launcher 启动器定义classLoader的范围
+     */
     public static void main(String[] args){
-        System.out.println(String.class.getClassLoader());
-        System.out.println(Test02_ClassLoaderLevel.class.getClassLoader());
-        System.out.println(Test02_ClassLoaderLevel.class.getClassLoader().getClass().getClassLoader());
+        String bootProperty = System.getProperty("sun.boot.class.path");
+        System.out.println(bootProperty.replaceAll(":",System.lineSeparator()));
+        System.out.println("--------------------------");
+        String extProperty = System.getProperty("java.ext.dirs");
+        System.out.println(extProperty.replaceAll(":",System.lineSeparator()));
+        System.out.println("--------------------------");
+        String appProperty = System.getProperty("java.class.path");
+        System.out.println(appProperty.replaceAll(":",System.lineSeparator()));
     }
 }
--- src/main/java/com/fang/jvm/test/Test03_LoadClass.java
+++ src/main/java/com/fang/jvm/test/Test03_LoadClass.java
@@ -3,21 +3,13 @@ package com.fang.jvm.test;
 /**
  * @author fanglingxiao
  * @version 1.0
- * @description classLoader范围
+ * @description 手动加载
  * @date 2021/7/13 12:40 上午
  **/
-public class Test02_ClassLoaderScope {
-    /**
-     * Launcher 启动器定义classLoader的范围
-     */
-    public static void main(String[] args){
-        String bootProperty = System.getProperty("sun.boot.class.path");
-        System.out.println(bootProperty.replaceAll(":",System.lineSeparator()));
-        System.out.println("--------------------------");
-        String extProperty = System.getProperty("java.ext.dirs");
-        System.out.println(extProperty.replaceAll(":",System.lineSeparator()));
-        System.out.println("--------------------------");
-        String appProperty = System.getProperty("java.class.path");
-        System.out.println(appProperty.replaceAll(":",System.lineSeparator()));
+public class Test03_LoadClass {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class clazz = Test03_LoadClass.class.getClassLoader().loadClass("com.fang.jvm.test.Test02_ClassLoaderScope");
+        System.out.println(clazz.getName());
     }
 }
