140ab632e7e784c154bc92ecfe8bd9dfa814ce9d
fanglingxiao <fanglingxiao@guoquan.cn>
2021-07-21 09:27:07 +0800

    ##classLoader

---
+++ src/main/java/com/fang/jvm/test/Test02_ClassLoaderScope.java
@@ -0,0 +1,15 @@
+package com.fang.jvm.test;
+
+/**
+ * @author fanglingxiao
+ * @version 1.0
+ * @description TODO
+ * @date 2021/7/13 12:40 上午
+ **/
+public class Test02_ClassLoaderLevel {
+    public static void main(String[] args){
+        System.out.println(String.class.getClassLoader());
+        System.out.println(Test02_ClassLoaderLevel.class.getClassLoader());
+        System.out.println(Test02_ClassLoaderLevel.class.getClassLoader().getClass().getClassLoader());
+    }
+}
