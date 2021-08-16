736ed228bb66ce2072f25c5eff3b62a860ba6135
fanglingxiao <fanglingxiao@guoquan.cn>
2021-08-17 01:31:38 +0800

    ##JMM-volatile原理

---
+++ src/main/java/com/fang/jvm/jmm/Test05_Volatile.java
@@ -0,0 +1,12 @@
+package com.fang.jvm.jmm;
+
+/**
+ * @author fanglingxiao
+ * @version 1.0
+ * @description TODO
+ * @date 2021/8/17 12:28 上午
+ **/
+public class Test05_Volatile {
+    int i;
+    volatile int j;
+}
