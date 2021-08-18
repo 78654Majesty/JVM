d51d402f69fa0edad463e1e9f52a6fb0ff093c30
fanglingxiao <fanglingxiao@guoquan.cn>
2021-08-18 10:09:18 +0800

    ##JMM-synchornized原理

---
+++ src/main/java/com/fang/jvm/jmm/Test06_Synchroniezd.java
@@ -0,0 +1,15 @@
+package com.fang.jvm.jmm;
+
+/**
+ * @author fanglingxiao
+ * @version 1.0
+ * @description synchronized原理分析
+ * @date 2021/8/17 10:46 下午
+ **/
+public class Test06_Synchroniezd {
+    public void test() {
+        synchronized (this) {
+
+        }
+    }
+}
