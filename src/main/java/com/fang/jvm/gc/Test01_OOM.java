f485f48770e6ef8a10486a952075d8f86a6afebb
fanglingxiao <fanglingxiao@guoquan.cn>
2021-09-22 09:46:36 +0800

    ##gc

--- src/main/java/com/fang/jvm/gc/Test01_OOM.java
+++ src/main/java/com/fang/jvm/gc/Test01_OOM.java
@@ -1,8 +1,25 @@
-package com.fang.jvm.gc;/**
- * @description TODO
+package com.fang.jvm.gc;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
  * @author fanglingxiao
- * @date 2021/9/12 6:59 下午
  * @version 1.0
+ * @description 模拟oom
+ * @date 2021/9/12 6:59 下午
  **/
 public class Test01_OOM {
+    /**
+     * -Xms10m -Xmx10m -XX:+PrintCommandLineFlags -XX:+PrintGCDetails
+     */
+    public static void main(String[] args) {
+        System.out.println("HeloGC!");
+        List<Byte[]> list = new LinkedList<>();
+        for (; ; ) {
+            // 1024*1024 = 1M
+            Byte[] b = new Byte[1024 * 1024];
+            list.add(b);
+        }
+    }
 }
--- src/main/java/com/fang/jvm/gc/Test02_FullGC_Problem01.java
+++ src/main/java/com/fang/jvm/gc/Test02_FullGC_Problem01.java
@@ -1,8 +1,61 @@
-package com.fang.jvm.gc;/**
- * @description TODO
+package com.fang.jvm.gc;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
  * @author fanglingxiao
- * @date 2021/9/22 1:22 上午
  * @version 1.0
+ * @description jvm启动参数
+ * -Xms200M -Xmx200M -XX:+PrintGC
+ * @date 2021/9/22 1:22 上午
  **/
 public class Test02_FullGC_Problem01 {
+
+    private static final ScheduledThreadPoolExecutor EXECUTOR = new ScheduledThreadPoolExecutor(50,
+            new ThreadPoolExecutor.DiscardOldestPolicy());
+
+    public static void main(String[] args) throws Exception {
+        EXECUTOR.setMaximumPoolSize(50);
+
+        for (; ; ) {
+            modelFit();
+            Thread.sleep(100);
+        }
+    }
+
+    private static class CardInfo {
+        BigDecimal price = BigDecimal.ZERO;
+        String name = "张三";
+        int age = 5;
+        Date birthdate = new Date();
+
+        public void m() {
+        }
+    }
+
+    private static void modelFit() {
+        List<CardInfo> taskList = getAllCardInfo();
+        taskList.forEach(info -> {
+            // do something
+            EXECUTOR.scheduleWithFixedDelay(() -> {
+                //do sth with info
+                info.m();
+            }, 2, 3, TimeUnit.SECONDS);
+        });
+    }
+
+    private static List<CardInfo> getAllCardInfo() {
+        List<CardInfo> taskList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            CardInfo ci = new CardInfo();
+            taskList.add(ci);
+        }
+        return taskList;
+    }
 }
