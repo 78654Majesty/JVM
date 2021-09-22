58948aaab0ee8a56d0f2c5cde44b89be4294f76d
fanglingxiao <fanglingxiao@guoquan.cn>
2021-09-22 09:46:00 +0800

    ##gc

---
+++ src/main/java/com/fang/jvm/gc/Test01_OOM.java
@@ -0,0 +1,8 @@
+package com.fang.jvm.gc;/**
+ * @description TODO
+ * @author fanglingxiao
+ * @date 2021/9/12 6:59 下午
+ * @version 1.0
+ **/
+public class Test01_OOM {
+}
---
+++ src/main/java/com/fang/jvm/gc/Test02_FullGC_Problem01.java
@@ -0,0 +1,8 @@
+package com.fang.jvm.gc;/**
+ * @description TODO
+ * @author fanglingxiao
+ * @date 2021/9/22 1:22 上午
+ * @version 1.0
+ **/
+public class Test02_FullGC_Problem01 {
+}
